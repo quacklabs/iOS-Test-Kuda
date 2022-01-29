77a174e46bbc098cab020b03be18e800e285542b
Mark Boleigha <donhoenix@gmail.com>
2022-01-29 01:08:25 +0100

    Initial Commit

---
+++ iOS-Test-kuda/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS-Test-kuda
+//
+//  Created by Mark Boleigha on 29/01/2022.
+//  Copyright © 2022 Umba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
